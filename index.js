709696610ccf4aee3c24cc0f79983f6689056d10
LuisGFonseca <lugustavobh@gmail.com>
2025-08-14 19:09:50 -0300

    add js

---
+++ index.js
@@ -0,0 +1,36 @@
+// pegar o valor do campo de email
+// verificar se o email nao e vazio e se o email e valido
+// se verdadeiro entao habilitar o botao de recuperar senha
+//se falso entao desabilitar o botao de recuperar senha
+function validateFields() {
+    console.log('validateFields chamada');
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+    
+    const password = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !password;
+
+// Função para validar o formato do email
+// Retorna true se o email for válido, false caso contrário
+    function isEmailValid() {
+        const email = document.getElementById("email").value;
+        if (!email) {
+            return false;
+        }
+        return validateEmail(email);
+    }
+
+
+// Função para validar o formato da senha
+    function isPasswordValid() {
+        const password = document.getElementById("password").value;
+        if(!password){
+            return false;
+        }
+        return true;
+    }
+
+    function validateEmail(email) {
+         return /\S+@\S+\.\S+/.test(email);
+    }
+}
